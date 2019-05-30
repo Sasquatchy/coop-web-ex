1a0b6756ef54fef9a63cc3f6c9abf1c353151a3e
안지훈 <Ahn-Laptop@DESKTOP-8PSL5IM>
2019-05-30 15:53:13 +0900

    Msg 서비스, DAO 추가

--- webex/src/org/zerock/domain/MsgVO.java
+++ webex/src/org/zerock/domain/MsgVO.java
@@ -1,18 +1,21 @@
 package org.zerock.domain;
 
+
 import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class MsgVO {
 
 	private int mno;
-	private String from, to, msg;
+	private String sender, whom, msg, Gubun;
 	
 	private Date regdate;
 }
